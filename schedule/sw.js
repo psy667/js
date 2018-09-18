5a8241d9442e782bd2664e32b803193e2355998b
Arseniy Oguzov <arseniyoguzov@gmail.com>
2018-09-19 02:44:59 +0300

    Update sw.js

--- schedule/sw.js
+++ schedule/sw.js
@@ -1,2 +1,14 @@
-'use strict';
-importScripts('sw-toolbox.js'); toolbox.precache(["index.html","style/style.css"]); toolbox.router.get('/images/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
+self.addEventListener('install', e => {
+ e.waitUntil(
+   // после установки service worker
+   // открыть новый кэш
+   caches.open('my-pwa-cache').then(cache => {
+     // добавляем все URL ресурсов, которые хотим закэшировать
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/style.css',
+     ]);
+   })
+ );
+});
